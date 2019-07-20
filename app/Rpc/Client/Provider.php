de0fe1a7b428007aedbb2d3f086f9c63e74b3faa
vagrant <vagrant@homestead>
2019-07-20 09:54:30 +0000

    Rpc测试

--- app/Rpc/Client/Provider.php
+++ app/Rpc/Client/Provider.php
@@ -21,15 +21,13 @@ class Provider implements ProviderInterface
      }*/
 
     public  function  getList(Client $client): array
-    {
+     {
 
           //var_dump($this->serviceName);
           //  bean('consulProvider')->getServerList($this->serviceName);
           //负载均衡，缓存地址
           //根据服务名称consul当中获取动态地址
-      /*$a = array('127.0.0.1:9501');
-          return $a;*/
-        //return ['127.0.0.1:9501'];
-        return [];
+          //return ["127.0.0.1:9501","127.0.0.1:9508"];
+          return [];
      }
 }
