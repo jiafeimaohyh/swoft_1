84860c614fdf6ff754c396c241aa693f4cb6f427
vagrant <vagrant@homestead>
2019-07-20 15:28:01 +0000

    consul注册服务。封装请求consulApi组件

--- app/Listener/RegisterService.php
+++ app/Listener/RegisterService.php
@@ -32,9 +32,9 @@ class RegisterService implements EventHandlerInterface
      */
     public function handle(EventInterface $event): void
     {
-        /*var_dump(bean('config')->get('provider.consul'));
+        var_dump(bean('config')->get('provider.consul'));
         var_dump(bean('consulProvider')->registerServer());
-        var_dump("注册服务");*/
+        var_dump("注册服务");
 
     }
 }
